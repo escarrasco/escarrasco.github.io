eef9475df765c88a6fd4e53964ebabcb2758b4d1
escarrasco <car18084@byui.edu>
2019-02-17 02:45:06 -0400

    lesson5

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,13 +1,15 @@
 function currentDate() {
     var d = new Date();
-    var weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
+    var weekday = new Array(
+                                "Sunday",
+                                "Monday",
+                                "Tuesday",
+                                "Wednesday",
+                                "Thursday",
+                                "Friday",
+                                "Saturday"
+                             );
+  
 
     var month = new Array(  "January",
                             "February",
