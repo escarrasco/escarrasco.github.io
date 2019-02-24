84d6485b0b59066c13204ca754931bd832baa109
escarrasco <car18084@byui.edu>
2019-02-24 03:07:22 -0400

    lesson7

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -2,5 +2,5 @@ function windChill(){
     var tempF = parseInt(document.getElementById('temperature').innerHTML);
     var speed = parseInt(document.getElementById('speed').innerHTML);
     var windChillFactor = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed,0.16) + 0.4275 * tempF * Math.pow(speed,0.16);
-    document.getElementById("windChill").innerHTML = windChillFactor;
+    document.getElementById("windChill").innerHTML = windChillFactor.toFixed(2);
 }
