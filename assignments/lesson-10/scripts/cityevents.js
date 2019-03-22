6e3b64181e20fa501785e19e70be4426d60dc158
escarrasco <car18084@byui.edu>
2019-03-21 20:17:57 -0400

    lesson10

--- assignments/lesson-10/scripts/cityevents.js
+++ assignments/lesson-10/scripts/cityevents.js
@@ -14,34 +14,35 @@
             var towns = jsonObj['towns'];
                 
             for (var i = 0; i < towns.length; i++) {
-                var myArticle = document.createElement('article');
-                var name = document.createElement('h3');
-                var motto = document.createElement('h4');
-                var  eventLabel = document.createElement('p');
-                var events = document.createElement('ul');
-                var picture = document.createElement('img');
-                picture.setAttribute('src', 'images/town' + i + '.jpeg');
-                picture.setAttribute('alt', 'town picture');
+                if(towns[i].name == 'Preston'){
+                    var myArticle = document.createElement('article');
+                    var name = document.createElement('h3');
+                    var motto = document.createElement('h4');
+                    var  eventLabel = document.createElement('p');
+                    var events = document.createElement('ul');
+                    var picture = document.createElement('img');
+                    picture.setAttribute('src', 'images/town' + i + '.jpeg');
+                    picture.setAttribute('alt', 'town picture');
 
-                name.textContent = towns[i].name;
-                motto.textContent = towns[i].motto;
-                eventLabel.textContent = 'Events: ';
-                
-                var eventsObj = towns[i].events;
-                for (var j = 0; j < eventsObj.length; j++) {
-                    var listItem = document.createElement('li');
-                    listItem.textContent = eventsObj[j];
-                    events.appendChild(listItem);
+                    name.textContent = towns[i].name;
+                    motto.textContent = towns[i].motto;
+                    eventLabel.textContent = 'Events: ';
+                    
+                    var eventsObj = towns[i].events;
+                    for (var j = 0; j < eventsObj.length; j++) {
+                        var listItem = document.createElement('li');
+                        listItem.textContent = eventsObj[j];
+                        events.appendChild(listItem);
+                    }
+
+                    myArticle.appendChild(picture);
+                    myArticle.appendChild(name);
+                    myArticle.appendChild(motto);
+                    myArticle.appendChild(eventLabel);
+                    myArticle.appendChild(events);
+                   
+                    section.appendChild(myArticle);
                 }
-
                 
-
-                myArticle.appendChild(name);
-                myArticle.appendChild(motto);
-                myArticle.appendChild(eventLabel);
-                myArticle.appendChild(events);
-                myArticle.appendChild(picture);
-
-                section.appendChild(myArticle);
             }
         }
